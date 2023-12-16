a6e4dc9fb0077b337e98a5d355eabc7185f60b2e
wing32048 <153213632+wing32048@users.noreply.github.com>
2023-12-16 17:29:27 +0800

    get source code failed

--- main.js
+++ main.js
@@ -1,12 +1,16 @@
-
-    window.stop();
-    fetch(window.location.href)
-        .then(response => response.text())
-        .then(data => {
-            // Process the response data
-            console.log(data);
-        })
-        .catch(error => {
-            // Handle any errors
-            console.error(error);
-        });
+chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+    if (tabs.length > 0) {
+      const tabId = tabs[0].id;
+      chrome.tabs.executeScript(tabId, { code: "document.documentElement.outerHTML" }, (result) => {
+        if (chrome.runtime.lastError) {
+          console.error(chrome.runtime.lastError);
+        } else if (result && result.length > 0) {
+          const pageSource = result[0];
+          console.log(pageSource);
+  
+          // Further processing or manipulation of the source code
+          // ...
+        }
+      });
+    }
+  });
