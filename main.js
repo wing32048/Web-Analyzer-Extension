870559c0a87ca2aa4a66270126bcd67888377c47
wing32048 <153213632+wing32048@users.noreply.github.com>
2023-12-19 22:01:50 +0800

    change check all values

--- main.js
+++ main.js
@@ -1,41 +1,29 @@
-if (sessionStorage.times % 2 === 0){
-    sessionStorage.setItem('check','true');
-    console.log("Checked");
-}else{
-    sessionStorage.times = 1;
-    window.stop()   
-    fetch("http://127.0.0.1/connect.php")
-        .then(response => response.json())
-        .then(malwarecode => {
-            // Process and use the retrieved data
-            fetch(window.location.href)
-                .then(response => response.text())
-                .then(data => {
-                    // Process the response data
-                    console.log(data);
-                    var $malweb = false;
-                    for (var i = 0; i < malwarecode.length; i++) {
-                        console.log(malwarecode[i]);
-                        if (data.includes(malwarecode[i])) {
-                            $malweb = true;
-                            console.log("This is malware website.");
-                            // console.log(i)
-                        }else {
-                            console.log("This is not malware website.");
-                        }
-                    }
-                    if ($malweb === false){
-                        window.location.reload();
-                        sessionStorage.times = Number(sessionStorage.times) +1;
-                    }
-                })
-                .catch(error => {
-                    // Handle any errors
-                    console.error(error);
-                });
-        })
-        .catch(error => {
-            // Handle any errors
-            console.error('Error retrieving data:', error);
-        });
-}
+var jsonCheck = {
+    base64: ["eval", "atob"],
+    DBD: ["createObjectURL", "click"]
+  };
+  
+  fetch(window.location.href)
+    .then(response => response.text())
+    .then(data => {
+      // Process the response data
+        console.log(data);
+        var malwareTypes = [];
+        var anyTypeObjectsIncluded = false;
+        for (var key in jsonCheck) {
+            var values = jsonCheck[key];
+            console.log(values);
+            var objectsIncluded = values.every(obj => data.includes(obj));
+            console.log(objectsIncluded);
+            if (objectsIncluded) {
+            anyTypeObjectsIncluded = true;
+            malwareTypes.push(key);
+            }
+        }
+        if (anyTypeObjectsIncluded) {
+            console.log("At least one type has all its objects included in the data.");
+            console.log("Malware types found:", malwareTypes.join(", "));
+        } else {
+            console.log("No type has all its objects included in the data.");
+        }
+    });
---
+++ main2.js
@@ -0,0 +1,45 @@
+if (sessionStorage.times % 2 === 0){
+    sessionStorage.setItem('check','true');
+    console.log("Checked");
+}else{
+    sessionStorage.times = 1;
+    window.stop()   
+    fetch("http://127.0.0.1/connect.php")
+        .then(response => response.json())
+        .then(malwarejs => {
+            // Process and use the retrieved data
+            console.log(window.location.href);
+            fetch(window.location.href)
+                .then(response => response.text())
+                .then(data => {
+                    // Process the response data
+                    console.log(data);
+                    var malwareTypes = [];
+                    var anyTypeObjectsIncluded = false;
+                    for (var key in malwarejs) {
+                        var values = malwarejs[key];
+                        console.log(values);
+                        var objectsIncluded = values.every(obj => data.includes(obj));
+                        console.log(objectsIncluded);
+                        if (objectsIncluded) {
+                        anyTypeObjectsIncluded = true;
+                        malwareTypes.push(key);
+                        }
+                    }
+                    if (anyTypeObjectsIncluded) {
+                        console.log("At least one type has all its objects included in the data.");
+                        console.log("Malware types found:", malwareTypes.join(", "));
+                    } else {
+                        console.log("No type has all its objects included in the data.");
+                    }  
+                })
+                .catch(error => {
+                    // Handle any errors
+                    console.error(error);
+                });
+        })
+        .catch(error => {
+            // Handle any errors
+            console.error('Error retrieving data:', error);
+        });
+}
