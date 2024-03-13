36f03eef671b924e3b382fb1ecf8e043dbf37ffd
wing32048 <153213632+wing32048@users.noreply.github.com>
2024-03-13 20:32:42 +0800

    Update popup.js

--- popup/html/popup.js
+++ popup/html/popup.js
@@ -1,7 +1,7 @@
 document.addEventListener("DOMContentLoaded", function() {
     var registerButton = document.getElementById("Setting");
     registerButton.addEventListener("click", function() {
-    chrome.tabs.create({ url:'http://127.0.0.1/ui/signin.php'});
+    chrome.tabs.create({ url:'http://192.168.140.100/ui/signin.php'});
     //   chrome.tabs.create({ url: "https://www.google.com" });
     });
   });
