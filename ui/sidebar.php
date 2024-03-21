80250d734a29fad9dfb1faae18b3b4cabb6f0daf
wing32048 <153213632+wing32048@users.noreply.github.com>
2024-03-21 14:11:24 +0800

    Update sidebar.php

--- ui/sidebar.php
+++ ui/sidebar.php
@@ -27,9 +27,7 @@
     </div>
 
     <!-- Nav Item - Pages Collapse Menu -->
-
-
-    <!-- Nav Item - Utilities Collapse Menu -->
+    
     <?php
         $pdo = dbconnect();
         $cookieId = $_COOKIE['id'];
@@ -41,7 +39,6 @@
             $result = $stmt->fetch();
             if ($result["admin"] == "Y"){
                 echo '
-                <!-- Nav Item - Pages Collapse Menu -->
                 <li class="nav-item">
                     <a class="nav-link" href="#" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="true"
                         aria-controls="collapseTwo">
@@ -56,48 +53,28 @@
                         </div>
                     </div>
                 </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#" data-toggle="collapse" data-target="#collapseUtilities"
-                        aria-expanded="true" aria-controls="collapseUtilities">
-                        <i class="fas fa-fw fa-wrench"></i>
-                        <span>Trend Analysis and Reporting</span>
-                    </a>
-                    <div id="collapseUtilities" class="collapse" aria-labelledby="headingUtilities"
-                        data-parent="#accordionSidebar">
-                        <div class="bg-white py-2 collapse-inner rounded">
-                            <h6 class="collapse-header">Reporting</h6>
-                            <a class="collapse-item" href="history.php">History</a>
-
-                            
-                        </div>
-                    </div>
-                </li>
                 ';
             }
-            else{
-                echo '
-                <!-- Nav Item - Utilities Collapse Menu -->
-                <li class="nav-item">
-                    <a class="nav-link" href="#" data-toggle="collapse" data-target="#collapseUtilities"
-                        aria-expanded="true" aria-controls="collapseUtilities">
-                        <i class="fas fa-fw fa-wrench"></i>
-                        <span>Trend Analysis and Reporting</span>
-                    </a>
-                    <div id="collapseUtilities" class="collapse" aria-labelledby="headingUtilities"
-                        data-parent="#accordionSidebar">
-                        <div class="bg-white py-2 collapse-inner rounded">
-                            <h6 class="collapse-header">Reporting</h6>
-                            <a class="collapse-item" href="history.php">History</a>
-
-                        </div>
-                    </div>
-                </li>
-                ';
-            }
+            
         } catch (PDOException $e) {
             die($e->getMessage());
         }
     ?>
+    <li class="nav-item">
+        <a class="nav-link" href="#" data-toggle="collapse" data-target="#collapseUtilities"
+            aria-expanded="true" aria-controls="collapseUtilities">
+            <i class="fas fa-fw fa-wrench"></i>
+            <span>Trend Analysis and Reporting</span>
+        </a>
+        <div id="collapseUtilities" class="collapse" aria-labelledby="headingUtilities"
+            data-parent="#accordionSidebar">
+            <div class="bg-white py-2 collapse-inner rounded">
+                <h6 class="collapse-header">Reporting</h6>
+                <a class="collapse-item" href="history.php">History</a>
+
+            </div>
+        </div>
+    </li>
 
 
     <!-- Divider -->
