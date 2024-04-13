e67822ff1a873f9a0a5437839c9da52f029724ac
wing32048 <wwings0218@gmail.com>
2024-04-13 23:41:35 +0800

    Update self_whitelist_history.php

--- htdocs/self_whitelist_history.php
+++ htdocs/self_whitelist_history.php
@@ -54,7 +54,7 @@
                     <p class="mb-4">Here can remove the whitelist</p>
                     <div class="card shadow mb-4">
                         <div class="card-header py-3">
-                            <h6 class="m-0 font-weight-bold text-primary">Whitelist</h6>
+                            <h6 class="m-0 font-weight-bold text-primary">Whitelist History</h6>
                         </div>
                         <div class="card-body">
                             <div class="table-responsive">
